89241209a921593f7a1da9660802631340fae431
Denys-Bardinov <denys.bardinov@nixsolutions.com>
2021-11-25 00:01:33 +0200

    Changes

--- src/test/java/tests/UI/SuiteTest.java
+++ src/test/java/tests/UI/SuiteTest.java
@@ -3,12 +3,13 @@ package tests.UI;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import tests.UI.authentication.Authentication;
+import tests.UI.events.Events;
 import tests.UI.search.SearchPhones;
 import tests.UI.search.SearchResultFacets;
 
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({SearchPhones.class, SearchResultFacets.class, Authentication.class})
-public class SuiteSearchTest {
+@Suite.SuiteClasses({SearchPhones.class, SearchResultFacets.class, Authentication.class, Events.class})
+public class SuiteTest {
 
 }
